440b6ec3ddf86b2de09457daf64c624f1cc73bda
wangdong-mi <wangdong-mi@togeek.cn>
2020-09-07 17:57:52 +0800

    新增

--- fusheng/cloud-provider-payment8001/src/main/java/cn/fusheng/springcloud/controller/PaymentController.java
+++ fusheng/cloud-provider-payment8001/src/main/java/cn/fusheng/springcloud/controller/PaymentController.java
@@ -4,6 +4,9 @@ import cn.fusheng.springcloud.entities.CommonResult;
 import cn.fusheng.springcloud.entities.Payment;
 import cn.fusheng.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -18,6 +22,12 @@ public class PaymentController {
 
    @Resource
    private PaymentService paymentService;
+
+   @Value("${server.port}")
+   private String serverPort;
+
+   @Resource
+   private DiscoveryClient discoveryClient;
 
    @PostMapping(value = "/payment/create")
    public CommonResult create(@RequestBody Payment payment)
@@ -44,5 +54,21 @@ public class PaymentController {
       }else{
          return new CommonResult(444,"没有对应记录,查询ID: "+id,null);
       }
+   }
+
+   @GetMapping(value = "/payment/discovery")
+   public Object discovery()
+   {
+      List<String> services = discoveryClient.getServices();
+      for (String element : services) {
+         log.info("*****element: "+element);
+      }
+
+      List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+      for (ServiceInstance instance : instances) {
+         log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+      }
+
+      return this.discoveryClient;
    }
 }
---
+++ fusheng/cloud-provider-payment8002/src/main/java/cn/fusheng/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,25 @@
+package cn.fusheng.springcloud.service.impl;
+
+import cn.fusheng.springcloud.dao.PaymentDao;
+import cn.fusheng.springcloud.entities.Payment;
+import cn.fusheng.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+   @Resource
+   private PaymentDao paymentDao;
+
+   @Override
+   public int create(Payment payment) {
+      return paymentDao.create(payment);
+   }
+
+   @Override
+   public Payment getPaymentById(Long id) {
+      return paymentDao.getPaymentById(id);
+   }
+}
